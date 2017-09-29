c04ace1f1eb7952410d5ca095c69bcd3f56f38b6
Neville Shane <nshane@ldeo.columbia.edu>
2017-09-29 15:01:14 -0400

    removed extra comma from AuthortypeCombo

--- app/store/datacite/combobox/AuthortypeCombo.js
+++ app/store/datacite/combobox/AuthortypeCombo.js
@@ -5,6 +5,6 @@ Ext.define('PMDMeta.store.datacite.combobox.AuthortypeCombo', {
     data: [
     				{abbr:'Author',name:'Author', qtip:'The primary author of the dataset.'},
 					{abbr:'CoAuthor',name:'Co-Author', qtip:'A co-author of the dataset.'},
-					{abbr:'Editor',name:'Editor', qtip:'A person who oversees the details related to the publication format of the resource. Note: if the Editor is to be credited in place of multiple authors, the Editor\'s name may be supplied as Author, with &quot;(Ed.)&quot; appended to the name. '},	
+					{abbr:'Editor',name:'Editor', qtip:'A person who oversees the details related to the publication format of the resource. Note: if the Editor is to be credited in place of multiple authors, the Editor\'s name may be supplied as Author, with &quot;(Ed.)&quot; appended to the name. '}
 			]				
 });
