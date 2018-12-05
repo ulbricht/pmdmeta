8cfcc11cdc594919bf3240551e361ee40e4720ef
ulbricht <ulbricht@gfz-potsdam.de>
2018-12-05 14:55:32 +0100

    suggest only valid layouts

--- resources/layoutlist.php
+++ resources/layoutlist.php
@@ -2,15 +2,16 @@
 
     include_once("includes/inc_conf.php");   
         
-    $reviewfiles=scandir("../../../");
+    $path="../../../";
+    $reviewfiles=scandir($path);
     
-    $invaliddirs=array(".","..","dmp","extern","panmetaworks","lib","alt");
     
    header("Content-type: text/xml");     
     echo "<html><body>";  
     foreach ($reviewfiles as $review)
-        if (array_search($review,$invaliddirs)===FALSE)
-            echo '<a href="'.$systemserver.$review.'" target="_blank">'.$review.'</a>'."\n";
+        if (is_dir($path.$review) && is_dir($path.$review."/xsl/") && file_exists($path.$review."/xsl/datasetoverview.xslt")){
+            echo '<a href="'.$systemserver."/".$review.'" target="_blank">'.$review.'</a>'."\n";
+	}
     echo "</body></html>";
             
 
